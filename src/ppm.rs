694eb87a769c3c97ff95131f4377c912923464fc
MROS <yc1043@gmail.com>
2019-04-23 19:05:24 +0800

    寫檔時先進一層緩衝區，再一起寫入 減少大量 IO 損耗

--- src/ppm.rs
+++ src/ppm.rs
@@ -1,16 +1,18 @@
 use crate::image::Image;
 
 use std::fs::File;
+use std::io::BufWriter;
 use std::io::prelude::*;
 
 pub fn to_ppm(image: Image) -> std::io::Result<()> {
-    let mut file = File::create("out.ppm")?;
+    let file = File::create("out.ppm")?;
+    let mut buffer = BufWriter::new(file);
 
-    write!(file, "P6\n{} {}\n255\n", image.width, image.height)?;
+    write!(buffer, "P6\n{} {}\n255\n", image.width, image.height)?;
     for row in 0..image.height {
         for col in 0..image.width {
             let pixel = &image.pixels[row as usize][col as usize];
-            file.write(&[pixel.r, pixel.g, pixel.b])?;
+            buffer.write(&[pixel.r, pixel.g, pixel.b])?;
         }
     }
     Ok(())
